d606e3d5e0b760d16372c2b68acfc025dba94d37
sampopitkanen <sampo.pitkanen@hurja.fi>
2024-01-12 12:08:51 +0200

    End of Section 2

---
+++ API/Program.cs
@@ -0,0 +1,19 @@
+using API.Data;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddControllers();
+builder.Services.AddDbContext<DataContext>(options =>
+{
+  options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
+
+var app = builder.Build();
+
+app.UseHttpsRedirection();
+app.UseAuthorization();
+app.MapControllers();
+
+app.Run();
