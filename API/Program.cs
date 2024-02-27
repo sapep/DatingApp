907095f3fb9b993512aface4d1c86092661c0c39
sampopitkanen <sampo.pitkanen@hurja.fi>
2024-02-27 11:39:31 +0200

    End of Section 3

--- API/Program.cs
+++ API/Program.cs
@@ -9,11 +9,17 @@ builder.Services.AddDbContext<DataContext>(options =>
 {
   options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
+builder.Services.AddCors();
 
 var app = builder.Build();
 
-app.UseHttpsRedirection();
-app.UseAuthorization();
+// Configure the HTTP request pipeline
+app.UseCors(builder => {
+  builder
+    .AllowAnyHeader()
+    .AllowAnyMethod()
+    .WithOrigins("https://localhost:4200");
+});
 app.MapControllers();
 
 app.Run();
